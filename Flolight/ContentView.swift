5c0809d676b6a032cbda810c0a690a8acde6d068
yurasera <yuhaya.lissera@gmail.com>
2026-08-01 17:32:26 +0700

    Initial Commit

---
+++ Flolight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flolight
+//
+//  Created by Yuhaya Lissera on 01/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flolight/FlolightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlolightApp.swift
+//  Flolight
+//
+//  Created by Yuhaya Lissera on 01/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FlolightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
